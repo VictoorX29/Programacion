1c12cc75d16b6011fa7c40fd8b53c54dfafd094c
Victor Martinez PC-Clase <victormn29@gmail.com>
2026-05-27 13:57:06 +0200

    Practica 8

---
+++ tutorial8-base-de-datos/Regalos/src/regalos/Regalos.java
@@ -0,0 +1,10 @@
+package regalos;
+import java.sql.*;
+
+public class Regalos {
+
+    public static void main(String[] args) {
+        Formulario f = new Formulario();
+        f.setVisible(true);
+    }
+}
